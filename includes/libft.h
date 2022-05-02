227faa2d7a19f22753a9531df956439c5823de66
Charles THERIN <ctherin@student.42.fr>
2022-05-02 19:17:19 +0200

    working strlcat, half broken strlcpy

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: ctherin <ctherin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 17:49:13 by ctherin           #+#    #+#             */
-/*   Updated: 2022/05/02 18:41:16 by ctherin          ###   ########.fr       */
+/*   Updated: 2022/05/02 19:14:01 by ctherin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,7 @@ size_t	ft_strlen(const char *s);
 void	*ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 
 #endif
