088b8b1911668c29f34364f92bd7ea883ce230f7
Charles THERIN <ctherin@student.42.fr>
2022-05-04 20:49:00 +0200

    memcmp

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: ctherin <ctherin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 17:49:13 by ctherin           #+#    #+#             */
-/*   Updated: 2022/05/04 20:20:54 by ctherin          ###   ########.fr       */
+/*   Updated: 2022/05/04 20:45:45 by ctherin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,6 @@ void	*ft_calloc(size_t nmemb, size_t size);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
