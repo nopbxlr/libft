348b3fd74c3e42432c03dd3f63cc8574f4dacb66
Charles THERIN <ctherin@student.42.fr>
2022-05-05 18:37:55 +0200

    fixed strrchr

--- srcs/ft_strrchr.c
+++ srcs/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: ctherin <ctherin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 21:27:54 by ctherin           #+#    #+#             */
-/*   Updated: 2022/05/04 21:41:01 by ctherin          ###   ########.fr       */
+/*   Updated: 2022/05/05 18:35:28 by ctherin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,13 +33,12 @@ void	*ft_strrchr(const void *s, int c)
 	len = ft_strlen(s) + 1;
 	if (!len)
 		return (NULL);
-	i = len - 1;
+	i = len;
 	ptr = (unsigned char *)s;
-	while (i > 0)
+	while (i--)
 	{
 		if (ptr[i] == (unsigned char)c)
 			return ((void *)ptr + i);
-		i--;
 	}
 	return (NULL);
 }
