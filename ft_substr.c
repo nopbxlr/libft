407b7acd1e32f07b5e386ab629d58ae5877883ae
Charles THERIN <ctherin@student.42.fr>
2022-05-09 22:07:49 +0200

    removed some leaks from substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ctherin <ctherin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 17:31:48 by ctherin           #+#    #+#             */
-/*   Updated: 2022/05/09 21:34:26 by ctherin          ###   ########.fr       */
+/*   Updated: 2022/05/09 22:05:04 by ctherin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,20 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (!s)
 		return (NULL);
 	d = (char *)s;
-	ptr = ft_calloc(len + 1, sizeof(char));
-	if (!ptr)
-		return (NULL);
 	if (start < ft_strlen(s))
 	{
+		if (ft_strlen(s) < len)
+			len = ft_strlen(s) - start;
+		ptr = ft_calloc(len + 1, sizeof(char));
+		if (!ptr)
+			return (NULL);
 		while (i < len && d[start + i])
 		{
 			ptr[i] = d[i + start];
 			i++;
 		}
+		return (ptr);
 	}
 	else
 		return (ft_calloc(1, sizeof(char)));
-	return (ptr);
 }
