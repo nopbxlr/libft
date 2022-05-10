abfe9222e223c560c1bc47cee49cf24b6fbb51e2
Charles THERIN <ctherin@student.42.fr>
2022-05-10 22:25:47 +0200

    removed extra function & capped makefile to 80 cols

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ctherin <ctherin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 17:49:13 by ctherin           #+#    #+#             */
-/*   Updated: 2022/05/10 21:49:47 by ctherin          ###   ########.fr       */
+/*   Updated: 2022/05/10 22:21:44 by ctherin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,6 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 void	*ft_calloc(size_t nmemb, size_t size);
-void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
